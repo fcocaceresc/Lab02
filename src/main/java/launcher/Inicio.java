9cb15c601ed3d7abcfa8233d22b90ebf47322545
Francisco <fcocacerescolihuil@gmail.com>
2025-06-19 09:32:04 -0400

    add console ui

--- src/main/java/launcher/Inicio.java
+++ src/main/java/launcher/Inicio.java
@@ -1,6 +1,10 @@
 package launcher;
-import view.JuegoDadosConsola;
-public class Inicio { public static void main(String[]args){
 
-}
+import view.JuegoDadosConsola;
+
+public class Inicio {
+    public static void main(String[] args) {
+        JuegoDadosConsola juegoDadosConsola = new JuegoDadosConsola();
+        juegoDadosConsola.menu();
+    }
 }
--- src/main/java/view/JuegoDadosConsola.java
+++ src/main/java/view/JuegoDadosConsola.java
@@ -1,4 +1,57 @@
 package view;
 
+import controller.JuegoDados;
+
+import java.util.Scanner;
+
 public class JuegoDadosConsola {
+    JuegoDados juegoDados = new JuegoDados();
+    Scanner scanner = new Scanner(System.in);
+
+    public void menu() {
+        boolean continuar = true;
+        while (continuar) {
+            mostrarOpciones();
+            String opcion = scanner.nextLine();
+            continuar = ejecutarOpcion(opcion);
+        }
+    }
+
+    public void mostrarOpciones() {
+        System.out.println("1. Lanzar dado");
+        System.out.println("2. Salir");
+    }
+
+    public boolean ejecutarOpcion(String opcion) {
+        switch (opcion) {
+            case "1":
+                jugar();
+                return true;
+            case "2":
+                return salir();
+            default:
+                System.out.println("Opción inválida");
+                return true;
+        }
+    }
+
+    public void jugar() {
+        int resultado = juegoDados.lanzarDados();
+        System.out.println("Resultado: " + resultado);
+        if (resultado == 7) {
+            System.out.println("Ganaste");
+        } else {
+            System.out.println("Perdiste");
+        }
+    }
+
+    public boolean salir() {
+        System.out.println("¿Está seguro que desea salir? (S/N)");
+        String respuesta = scanner.nextLine().trim();
+        if (respuesta.equals("S")) {
+            System.out.println("Saliendo");
+            return false;
+        }
+        return true;
+    }
 }
