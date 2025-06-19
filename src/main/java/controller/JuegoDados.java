b37ee7ef60b199460a6ccc7b7e9823602fc8b324
Francisco <fcocacerescolihuil@gmail.com>
2025-06-19 09:50:44 -0400

    add juego dados controller

--- src/main/java/controller/JuegoDados.java
+++ src/main/java/controller/JuegoDados.java
@@ -1,4 +1,19 @@
 package controller;
 
+import model.Dado;
+
 public class JuegoDados {
+    private Dado dado1;
+    private Dado dado2;
+
+    public JuegoDados() {
+        dado1 = new Dado();
+        dado2 = new Dado();
+    }
+
+    public int lanzarDados() {
+        dado1.lanzar();
+        dado2.lanzar();
+        return dado1.getCaraSuperior() + dado2.getCaraSuperior();
+    }
 }
