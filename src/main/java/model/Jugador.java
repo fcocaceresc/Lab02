1e1c45ea4d58ec1f6b42de5dbb3500b8693612d1
GatoMiau03 <d.benaldo03@ufromail.cl>
2025-06-19 10:20:31 -0400

    Creada la clase Jugador basado en juegoDados

--- src/main/java/model/Jugador.java
+++ src/main/java/model/Jugador.java
@@ -1,4 +1,21 @@
 package model;
 
+import model.Dado;
+
+import java.util.Random;
+
 public class Jugador {
+    private Dado dado1;
+
+    public Jugador() {
+        dado1 = new Dado();
+    }
+    private void lanzarDado(Dado dado) {
+        dado.setCaraSuperior(new Random().nextInt(6) + 1);
+    }
+    public int lanzamientoJugador(){
+    lanzarDado(dado1);
+        return dado1.getCaraSuperior();
+    }
+
 }
